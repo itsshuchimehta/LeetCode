5460b68b81fbf56026735c1f221661430f90ea44
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2023-12-29 19:18:31 -0500

    Update solution.py

--- find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,45 +1,3 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        result = []
-        first = self.first_position(nums, target)
-
-        #If target is not found in the array, return [-1, -1].
-        if first == -1:
-            return [-1, -1]
-        
-        result.append(first)
-        last = self.last_position(nums, target)
-        result.append(last)
-        return result
-   
-    #find the starting position of a given target value.
-    def first_position(self,nums,target):
-        def condition(mid):
-            if nums[mid] == target:
-                if mid > 0 and nums[mid - 1] == target:
-                    return 'left'
-                else:
-                    return 'found'
-            elif nums[mid] > target:
-                return 'left'
-            else:
-                return 'right'        
-        return helper_search(0, len(nums)-1, condition)
-
-    #find the ending position of a given target value.
-    def last_position(self,nums,target):
-        def condition(mid):
-            if nums[mid] == target:
-                if mid < len(nums)-1 and nums[mid + 1] == target:
-                    return 'right'
-                else:
-                    return 'found'
-            elif nums[mid] > target:
-                return 'left'
-            else:
-                return 'right'        
-        return helper_search(0, len(nums)-1, condition)    
-
 #helper function 
 def helper_search(lo,hi,condition):
     while lo<= hi:
@@ -53,3 +11,44 @@ def helper_search(lo,hi,condition):
         elif result == 'right':
             lo = mid + 1
     return -1
+#find the starting position of a given target value.
+def first_position(nums,target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid > 0 and nums[mid - 1] == target:
+                return 'left'
+            else:
+                return 'found'
+        elif nums[mid] > target:
+            return 'left'
+        else:
+            return 'right'        
+    return helper_search(0, len(nums)-1, condition)
+
+#find the ending position of a given target value.
+def last_position(nums,target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid < len(nums)-1 and nums[mid + 1] == target:
+                return 'right'
+            else:
+                return 'found'
+        elif nums[mid] > target:
+            return 'left'
+        else:
+            return 'right'        
+    return helper_search(0, len(nums)-1, condition)   
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        first = first_position(nums, target)
+
+        #If target is not found in the array, return [-1, -1].
+        if first == -1:
+            return [-1, -1]
+        
+        result.append(first)
+        last = last_position(nums, target)
+        result.append(last)
+        return result
