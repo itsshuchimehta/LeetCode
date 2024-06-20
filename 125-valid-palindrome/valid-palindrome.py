5a3a01a3b51e8b7300765109a1ef8555b64154c6
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-06-20 17:51:37 -0400

    Time: 51 ms (25.64%) | Memory: 23.1 MB (5.75%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        original = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        n = len(original)
+        if n == 0:
+            return True
+        # print(original)
+        return self.helper(original, 0, n)
+    
+    def helper(self, s, i, n):
+        
+        if i >= n//2:
+            return True
+        if s[i] != s[n-i-1]:
+            return False
+        return self.helper(s, i+1, n)    
