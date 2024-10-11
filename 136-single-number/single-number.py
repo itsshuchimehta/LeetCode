878528baa26f6dba3559762a6009b913da7e86a6
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-10-10 21:03:53 -0400

    Time: 121 ms (19.57%) | Memory: 19.3 MB (10.61%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashArr = {}
+
+        for num in nums:
+            if num not in hashArr:
+                hashArr[num] = 1
+            else:
+                hashArr[num] = hashArr[num] + 1
+            
+        
+        Anskey = [k for k, v in hashArr.items() if v == 1]   
+
+        print(hashArr)
+
+        return Anskey[0]
