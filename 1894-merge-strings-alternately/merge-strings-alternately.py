3c96a73f2e6724e5f5a6fe2b281613ae83de1b15
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-10-28 22:24:27 -0400

    Time: 45 ms (6.59%) | Memory: 16.7 MB (10.50%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,26 +1,20 @@
-# You are given two strings word1 and word2. 
-# Merge the strings by adding letters in alternating order, 
-# starting with word1. If a string 
-# is longer than the other, append the additional letters 
-# onto the end of the merged string.
-
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        merge =[]
-        i, j = 0, 0
+        i,j = 0, 0
+        merged = []
 
         while i < len(word1) and j < len(word2):
-            merge.append(word1[i])
-            merge.append(word2[j])
-            i += 1
-            j += 1
+            merged.append(word1[i])
+            merged.append(word2[j])
+            i+=1
+            j+=1
 
         while i < len(word1):
-            merge.append(word1[i])
-            i += 1
-            
+            merged.append(word1[i])
+            i+=1
+        
         while j < len(word2):
-            merge.append(word2[j])
-            j += 1
-            
-        return ''.join(merge)
+            merged.append(word2[j])
+            j+=1
+        
+        return ''.join(merged)
