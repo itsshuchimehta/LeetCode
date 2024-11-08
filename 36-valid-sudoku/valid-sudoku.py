e8ed9e391f2cca45cb8dda1a666abd0f990d7bcf
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-11-07 21:37:05 -0500

    Time: 7 ms (40.07%) | Memory: 16.8 MB (8.20%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -2,19 +2,18 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
-        square = collections.defaultdict(set) #[r/3, c/3]
+        squares = collections.defaultdict(set) #key = (r/3, c/3)
 
         for r in range(9):
             for c in range(9):
-
                 if board[r][c] == ".":
                     continue
-                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[(r//3,c//3)]):
-                    return False
-                
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False #Found duplicate in either of the sets
+                # Else Add that in the sets
                 cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
-                square[(r//3,c//3)].add(board[r][c])
-
+                squares[(r//3, c//3)].add(board[r][c])
         return True
-             
