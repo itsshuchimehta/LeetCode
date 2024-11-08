91a8e44185106bec6ba6f9f68c1b1199f7ea44f3
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-11-07 22:08:18 -0500

    Time: 18 ms (38.66%) | Memory: 21.6 MB (26.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,11 +4,11 @@ class Solution:
         anagrams_dict = defaultdict(list)
 
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26      # Array to count each letter in the string
             for c in s:
-                count[ord(c) - ord('a')] += 1
-            
-            key = tuple(count)
+                count[ord(c) - ord('a')] += 1 # Count characters
+             
+            key = tuple(count) # Use the count as a tuple as the key
             anagrams_dict[key].append(s)
         
         return list(anagrams_dict.values())
