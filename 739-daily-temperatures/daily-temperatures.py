060cd0a263f211031625cfc74460cc9f0deaf352
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-11-14 22:52:24 -0500

    Time: 126 ms (36.40%) | Memory: 32.7 MB (16.95%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -3,12 +3,8 @@ class Solution:
         answer = [0] * len(temperatures)
         stack = []
         for i, t in enumerate(temperatures):
-            if not stack:
-                stack.append((t, i))
-            else:
-                while stack and stack[-1][0] < t:
-                    current = stack.pop()
-                    answer[current[1]] = i - current[1]
-                   
-                stack.append((t,i))
+            while stack and stack[-1][0] < t:
+                stk_temp, stk_inx = stack.pop()
+                answer[stk_inx] = i - stk_inx
+            stack.append((t,i))
         return answer
