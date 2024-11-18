ac65cc625bd4d744305fc6dbd40c0c06a6eedf06
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-11-17 19:38:55 -0500

    Time: 2 ms (17.29%) | Memory: 16.8 MB (8.94%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,52 +1,24 @@
-"""
-You are given the heads of two sorted linked lists list1 and list2.
-
-Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
-
-Return the head of the merged linked list.
-
-
-"""
-
 # Definition for singly-linked list.
-
-class ListNode:
-    def __init__(self, val = 0, nxt = None):
-        self.val = val
-        self.next = nxt
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        current = dummy
-
+        d = ListNode() 
+        cur = d
         while list1 and list2:
             if list1.val < list2.val:
-                current.next = list1
+                cur.next = list1
+                cur = list1
                 list1 = list1.next
             else:
-                current.next = list2
+                cur.next = list2
+                cur = list2
                 list2 = list2.next
-            current= current.next
-
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
         
-        return dummy.next
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        cur.next = list1 if list1 else list2
+        
+        return d.next
+             
+                
