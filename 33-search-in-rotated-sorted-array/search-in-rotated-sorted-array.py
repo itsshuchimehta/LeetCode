7211facd9327a8e504685517b291616b0c4af528
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-11-21 20:26:08 -0500

    Time: 0 ms (100.00%) | Memory: 17 MB (57.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -4,6 +4,7 @@ class Solution:
         L = 0
         R = n - 1
 
+        # Check if the array is rotated, if it is get the index of min of the array
         while L < R:
             mid = L + ((R - L)//2)
             if nums[mid] > nums[R]:
@@ -13,6 +14,7 @@ class Solution:
         
         min_index = L
 
+        # Set the index range where the target could be
         if min_index == 0:
             L, R = 0, n - 1 
         elif target >= nums[0] and target <= nums[min_index - 1]:
@@ -20,6 +22,7 @@ class Solution:
         else:
             L, R = min_index, n - 1
 
+        # Search for target within that range
         while L <= R:
             mid = L + ((R - L)//2)
             if nums[mid] == target:
@@ -30,17 +33,6 @@ class Solution:
                 R = mid - 1
         
         return -1
-            
-        # while L <= R:
-        #     mid = L + ((R-L) // 2)
-        #     if nums[mid] == target:
-        #         return mid
-        #     elif nums[mid] > nums[R]: 
-        #         if nums[R] > target:
-        #             L = mid
-        #         else:
-        #             L = mid + 1
-        #     else:
-        #         R = mid - 1     
-        
-        # return -1
+
+        # TC: O(2 log(n)) == O(lon(n))
+        # SC: O(1)
