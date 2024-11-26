b07edf2eda0d43c8704e5b02ba66a567885a8d3f
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-11-26 01:06:28 -0500

    Time: 0 ms (100.00%) | Memory: 16.9 MB (5.74%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -1,4 +1,3 @@
-from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,6 +6,7 @@ from collections import deque
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # Recursively
         def same(root1, root2):
             if not root1 and  not root2:
                 return True
@@ -17,7 +17,35 @@ class Solution:
             
             return same(root1.left, root2.right) and same(root1.right, root2.left)
         
-        return same(root, root)
+        # return same(root, root)
         
+        # iteratively
+
+        stk = [root, root]
+
+        while stk:
+            node1 = stk.pop()
+            node2 = stk.pop()
+            
+            if not node1 or not node2:
+                return False
+
+            if node1.val != node2.val:
+                return False
+            
+            if (node1.left and not node2.right) or (node1.right and not node2.left) or (not node1.left and node2.right) or (not node1.right and  node2.left):
+                return False
+
+            if node1.left and node2.right:
+                stk.append(node2.right)
+                stk.append(node1.left)
+            if node1.right and node2.left:
+                stk.append(node2.left)
+                stk.append(node1.right)
+
+            
+        
+        return True
+
 
             
