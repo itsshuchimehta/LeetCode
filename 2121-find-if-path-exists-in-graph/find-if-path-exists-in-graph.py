85af09d840feb4300a38a39118c2b70ac3c0cf30
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-12-06 02:15:25 -0500

    Time: 579 ms (45.39%) | Memory: 139.4 MB (17.32%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # Using DSF (Recursion)
+        if source == destination:
+            return True
+        
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        seen = set()
+        seen.add(source)
+
+        def dfs(i):
+            if i == destination:
+                return True
+            for nei_node in graph[i]:
+                if nei_node not in seen:
+                    seen.add(nei_node)
+                    if dfs(nei_node):
+                        return True
+            return False
+
+        return dfs(source)
+        # TC: O(V+E)
+        # SC: O(V+E)
