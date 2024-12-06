f7909e1ee25b39a1d65d404ddc68ced4d0850540
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2024-12-06 02:21:24 -0500

    Time: 295 ms (91.30%) | Memory: 96.2 MB (67.79%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,6 +1,7 @@
+from collections import deque
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # Using DSF with stack (Iterative)
+        # Using BFS with Queue (Iterative)
         if source == destination:
             return True
         
@@ -11,14 +12,15 @@ class Solution:
             graph[v].append(u)
         seen = set()
         seen.add(source)
-        stk = [source]
+        q = deque()
+        q.append(source)
 
-        while stk:
-            node = stk.pop()
+        while q:
+            node = q.popleft()
             for nei_node in graph[node]:
                 if nei_node not in seen:
                     seen.add(nei_node)
-                    stk.append(nei_node)
+                    q.append(nei_node)
                 
                 if nei_node == destination:
                     return True
