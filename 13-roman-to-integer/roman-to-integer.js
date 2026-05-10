4829a4878f0373b59a2e8a0a60516357af63eb6a
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2026-05-10 14:35:36 -0400

    Time: 4 ms (73.30%) | Memory: 60.9 MB (67.52%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let romanToDigit  = {
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    }
+    let i = 0;
+    let summ = 0;
+    while(i < s.length){
+        if(romanToDigit[s[i]] < romanToDigit[s[i+1]]){
+            summ = summ + (romanToDigit[s[i+1]] - romanToDigit[s[i]]);
+            i+=2;
+        }else{
+            summ += romanToDigit[s[i]];
+            i++;
+        }
+    }
+
+    return summ;
+};
