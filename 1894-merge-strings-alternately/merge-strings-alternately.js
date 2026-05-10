bdff40fc7e46cec5588697ad1f6d712efac829d0
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2026-05-10 14:11:56 -0400

    Time: 48 ms (34.66%) | Memory: 54.1 MB (45.12%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,53 +4,24 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    // const arrWord1 = word1.split("");
-    // const arrWord2 = word2.split("");
-    // let merged = [];
-    // const minLen = Math.min(word1.length, word2.length);
+    let merged = "";
 
-    // // Loop through the overlapping part
-    // for(let i = 0; i < minLen; i++) {
-    //     merged.push(word1[i]);
-    //     merged.push(word2[i]);
-    // }
+    let i = 0;
 
-    // // Append the remainder directly from the string
-    // if(arrWord1.length > minLen){
-    //     merged.push(word1.slice(minLen))
-    // }
-    // if(arrWord2.length > minLen){
-    //     merged.push(word2.slice(minLen))
-    // }
+    while(i < word1.length && i < word2.length){
+        merged += word1[i] + word2[i];
+        i++;
+    }
 
-    const [A, B] = [word1.length, word2.length];        // const A = word1.length, B = word2.length;
-    let [a, b] = [0 , 0];                               // ->  let a = 0, b = 0;
-    
-    let merged = [];
+    if(word1.length !== word2.length){
 
-    let word = 1;                                       //keep track of which word 
-    while( a < A && b < B){
-        if(word === 1){
-            merged.push(word1[a]);
-            a++;
-            word = 2;
+        if(word1.length > word2.length){
+            merged += word1.slice(i)
         }else{
-            merged.push(word2[b]);
-            b++;
-            word = 1;
+            merged += word2.slice(i)
         }
     }
 
-    while(a < A){
-        merged.push(word1[a]);
-        a++;
-    }
-    
-    while(b < B){
-        merged.push(word2[b]);
-        b++;
-    }
-    
 
-    return merged.join("")
+    return merged;
 };
