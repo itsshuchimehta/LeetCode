73517804e6733efaae994adf19ae61b205e49ea8
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2026-05-11 16:05:01 -0400

    Time: 6 ms (16.45%) | Memory: 67.7 MB (5.58%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -6,13 +6,13 @@ var maxProfit = function(prices) {
     let minPrice = Infinity;
     let maxProfit = 0;
 
-    prices.forEach((price) => {
+    for(const price of prices) {
         if(price < minPrice){
             minPrice = price;
         }else if(price - minPrice > maxProfit){
             maxProfit = price - minPrice;
         }        
-    });
+    }
 
     return maxProfit; 
 };
