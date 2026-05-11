9523136841f567984ce09de20eb757809564e9c6
Shuchi Mehta <115120084+itsshuchimehta@users.noreply.github.com>
2026-05-11 15:45:57 -0400

    Time: 0 ms (100.00%) | Memory: 55.7 MB (44.74%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,16 +4,26 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    
+    if(s.length === 0){
+        return true;
+    }
+    if(s.length > t.length){
+        return false;
+    }
+
     let p = 0;
     
-    for (let i = 0; i < t.length && p < s.length; i++) {
-        if (s[p] === t[i]) {
-            p++; 
+
+    for(let i = 0; i < t.length; i++){
+        if(s[p] === t[i]){
+            if(p === s.length - 1){
+                return true;
+            }
+            p++;
         }
     }
 
-    return p === s.length;
+    return false;
 
 
 };
